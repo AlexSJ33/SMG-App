a0d7d4458eea473d0e19f4284f941c6d9cb5d974
Alex Santos <alevildark@gmail.com>
2022-12-10 21:46:26 -0300

    Ajustes no arquivo main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen,ScreenManager
+from kivymd.uix.card import MDCard
+from kivymd.uix.label import MDLabel
+from kivymd.uix.dialog import MDDialog
+
 from kivy.core.window import Window
 from database import ConectaBanco
 
@@ -16,10 +20,27 @@ class GerenciadorTelas(ScreenManager):
 class Login(Screen):
     pass
 
+class ListUser(MDCard):
 
+    
+    def exibir_dados(self):
+        lista = c.listar_dados()
+        for users in lista:
+                    self.ids['box'].add_widget(
+                        MDLabel(
+                            text=f"{users[1]}",
+                            halign="left",
+                            
+                            adaptive_height=True,
+                        )
+                    )
+    
+    def fechar(self):
+        self.parent.remove_widget(self)
+    
 
-class Account(Screen,ConectaBanco):
-        
+class Account(Screen,ConectaBanco,MDApp):
+    dialog = None    
     def pega_valor(self):
         username = self.ids.username.text
         username = "".join(username.split())
@@ -32,21 +53,34 @@ class Account(Screen,ConectaBanco):
         data = (username,email,password)
         
         if username == '' or email.strip() == '' or password.strip() == '' or password_2.strip() == '':
-            print('Necessario prencher todos os dados')
+            self.dialog = MDDialog(
+                text="[color=#f9f9f9]Preencha todos os campos ![/color]",
+                md_bg_color='3c3c3c',
+                )
+            self.dialog.open()
+            
         else:
-            print(data)
-        #return c.inserir_dados(data)
-
+            
+            return c.inserir_dados(data)
+    
+    def abrir_card(self):
+        self.add_widget(ListUser())
+        
 class MyApp(MDApp):
 
     def build(self):
         self.theme_cls.primary_palette = 'Purple'
         self.theme_cls.theme_style = 'Dark'
         return Builder.load_file('app.kv')
+    
 
 c=ConectaBanco()
 c.connect()
 c.create_table()
+c.listar_dados()
+
+if __name__=='__main__':
+    MyApp().run()
 
 
-MyApp().run()
+
