9dc2687b3ee1b2f3b51c7f932db2a678037cead3
AlexSJ <alevildark@gmail.com>
2023-01-02 20:22:18 -0300

    Atualizando cadastro

--- app/main.py
+++ app/main.py
@@ -85,13 +85,42 @@ class Login(Screen):
                 self.ids.label_login.text_color = 'red'
     
 class CadastrarCliente(MDCard):
+    def get_cliente(self):
+        nome = self.ids.nome.text
+        cpf = self.ids.cpf.text
+        email = self.ids.email.text
+        email = "".join(email.split())
+        telefone = self.ids.telefone.text
+        telefone = "".join(telefone.split())
+        cep = self.ids.cep.text
+        cep = "".join(cep.split())
+        cidade = self.ids.cidade.text
+        uf = self.ids.uf.text
+        uf = "".join(uf.split())
+        endereco = self.ids.endereco.text
+        numero = self.ids.numero.text
+        numero = "".join(numero.split())
+        bairro = self.ids.bairro.text
+
+        if nome == '' or cpf.strip() == '' or email.strip() == '' \
+            or telefone.strip() == '' or cep.strip() == '' or cidade == '' \
+            or uf.strip() =='' or endereco =='' or bairro =='':
+
+            self.dialog = MDDialog(
+                text="[color=#f9f9f9]Preencha todos os campos ![/color]",
+                md_bg_color='3c3c3c',
+                )
+            self.dialog.open()
+        else:
+            print('ok')
+
 
     def fechar(self):
         self.parent.remove_widget(self)   
 
 class CadastrarUsuario(MDCard):
     dialog = None    
-    def pega_valor(self):
+    def get_usuario(self):
         usuario = self.ids.usuario.text
         usuario = "".join(usuario.split())
         email = self.ids.email.text
